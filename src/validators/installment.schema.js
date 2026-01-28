a05847308eb79772962ce9e294a1d0b20774c06b
AbhishekPatil1310 <ap7497493@gmail.com>
2026-01-28 13:32:12 +0530

    no installment added

--- src/validators/installment.schema.js
+++ src/validators/installment.schema.js
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const createInstallmentSchema = z.object({
-  months: z.enum(["1", "2", "3","4","NO Limit"]),
+  months: z.enum(["1", "2", "3","4","NO Limit","No installment"]),
   surcharge: z.number().min(0),
 });
 
